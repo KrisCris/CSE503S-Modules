ad87fc72b614e0ed019131ce25fece302f36aa6f
connlost <hi.imhpc@outlook.com>
2021-09-17 21:59:09 -0500

    ok, i think this is the only way to perform logic in webpages without js

---
+++ view/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="POST">
+        <input type="text" name="username" id="username" placeholder="Username">
+        <div>
+            <input type="radio" name="regorlog" id="" value="login"><label for="login">Login</label><br>
+            <input type="radio" name="regorlog" id="" value="register"><label for="register">Register</label><br>            
+        </div>
+        <input type="submit" value="Go">
+    </form>
+</body>
+</html>
+form
+<?php
+    if(isset($_POST["username"]) && isset($_POST["regorlog"])){
+        $type = $_POST["regorlog"];
+        $username = $_POST["username"];
+        if($type == "login"){
+            
+        } elseif($type == "register"){
+            
+        }
+    } else {
+        exit;
+    }
+?>
