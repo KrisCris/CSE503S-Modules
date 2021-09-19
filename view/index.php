420a32ca71aef27c796768cf677fc077462f2650
Pingchuan Huang <hi.imhpc@outlook.com>
2021-09-19 02:05:35 -0500

    fix syntax error on htmls

--- view/index.php
+++ view/index.php
@@ -10,56 +10,59 @@
 </head>
 
 <body>
+    <?php
+    session_start();
+    require dirname(__FILE__) . '/../api/model/DataManager.php';
+    $DM = DataManager::getInstance();
+    if (isset($_SESSION["username"]) && $DM->hasUser($_SESSION["username"])) {
+        header("Location: my_files.php");
+    }
+    ?>
     <div>
-    <img src="res/icon.gif"/>
+        <img alt="icon" src="res/icon.gif" />
     </div>
     <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="POST">
         <input type="text" name="username" id="username" placeholder="Username">
         <div>
-            <input type="radio" name="regorlog" id="" value="login" checked><label for="login">Login</label><br>
-            <input type="radio" name="regorlog" id="" value="register"><label for="register">Register</label><br>
+            <input type="radio" name="regorlog" id="login" value="login" checked><label for="login">Login</label><br>
+            <input type="radio" name="regorlog" id="register" value="register"><label for="register">Register</label><br>
         </div>
         <input id="btn" type="submit" value="Go">
     </form>
-</body>
-<p id="err">
-    <?php
-    require dirname(__FILE__).'/../api/model/DataManager.php';
-    $DM = DataManager::getInstance();
-    if ($DM != null) {
-        if (isset($_POST["username"]) && isset($_POST["regorlog"])) {
-            $type = $_POST["regorlog"];
-            $username = $_POST["username"];
-            if ($username == "") {
-                echo "INVALID INPUT";
-                exit;
-            }
-            # try to check account status, 
-            # but indeed these code are useless as no protection are added to any account.
-            if ($type == "login") {
-                if ($DM->hasUser($username)) {
-                    session_start();
-                    $_SESSION["username"] = $username;
-                    header("Location: my_files.php");
-                } else {
-                    echo "YOU HAVE TO REGISTER FIRST!";
+    <p id="err">
+        <?php
+        if ($DM != null) {
+            if (isset($_POST["username"]) && isset($_POST["regorlog"])) {
+                $type = $_POST["regorlog"];
+                $username = $_POST["username"];
+                if ($username == "") {
+                    echo "INVALID INPUT";
                     exit;
                 }
-            } elseif ($type == "register") {
-                if ($DM->addUser($username)) {
-                    session_start();
-                    $_SESSION["username"] = $username;
-                    header("Location: my_files.php");
-                } else {
-                    echo "Username Existed!";
-                    exit;
+                # try to check account status, 
+                # but indeed these code are useless as no protection are added to any account.
+                if ($type == "login") {
+                    if ($DM->hasUser($username)) {
+                        $_SESSION["username"] = $username;
+                        header("Location: my_files.php");
+                    } else {
+                        echo "YOU HAVE TO REGISTER FIRST!";
+                        exit;
+                    }
+                } elseif ($type == "register") {
+                    if ($DM->addUser($username)) {
+                        $_SESSION["username"] = $username;
+                        header("Location: my_files.php");
+                    } else {
+                        echo "Username Existed!";
+                        exit;
+                    }
                 }
             }
         }
-    }
+        ?>
+    </p>
+</body>
 
-
-    ?>
-</p>
 
 </html>
--- view/my_files.php
+++ view/my_files.php
@@ -15,13 +15,20 @@
         <p>HELLO WORLD!</p>
         <p>Dear
             <span id="username">
-                <?php session_start();
+                <?php
+                session_start();
                 if (!isset($_SESSION["username"])) {
                     header("Location: index.php");
+                } else {
+                    $username = $_SESSION["username"];
+                    require dirname(__FILE__) . '/../api/model/DataManager.php';
+                    $DM = DataManager::getInstance();
+                    if (!$DM->hasUser($username)) {
+                        header("Location: index.php");
+                    }
                 }
                 echo $_SESSION["username"];
-                $username = $_SESSION["username"];
-                require dirname(__FILE__) . "/../api/IOUtil.php";
+
                 ?>
             </span>
             :)
@@ -31,21 +38,11 @@
         <input id="logout" type="submit" value="Logout">
     </form>
     <div id="filelist">
-        <p id="filelistprompt">Following Are Your Files!</p>
-        <form id="upload" action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="post" enctype="multipart/form-data">
-            <input type="file" name="file" id="file"><br>
-            <input type="submit" value="Upload">
-            <?php
-            # Upload
-            if (isset($_FILES["file"])) {
-                $file = $_FILES["file"];
-                echo IOUtil::saveFile($username, $file) ? '' : 'An Error Occured!';
-            }
-            ?>
-        </form>
+        <p class="filelistprompt">Following Are Your Files!</p>
+        <p></p>
         <div>
             <?php
-            # Download/Preview
+            # File manipulation
             if (isset($_POST["filePath"])) {
                 $fpath = $_POST["filePath"];
                 if (isset($_POST["preview"]) && $_POST["preview"] == 1) {
@@ -60,43 +57,122 @@
                     IOUtil::removeFile($username, $fpath);
                     # no exit; since we want to have file list refreshed
                 }
+                if (isset($_POST["unzip"]) && $_POST["unzip"] == 1) {
+                    IOUtil::unzip($username, $fpath);
+                    # no exit; since we want to have file list refreshed
+                }
             }
-
             # File Listing
             $innerPath = "";
             if (isset($_POST["innerPath"])) {
                 $innerPath = $_POST["innerPath"];
+                $innerPath = rawurldecode($innerPath);
             }
             $containerPath = IOUtil::$path . $username . "/" . $innerPath;
+            $finfo = new finfo(FILEINFO_MIME_TYPE); ?>
+
+            <p class="filelistprompt">Current Path: <?php echo $containerPath; ?></p>
+
+            <form id="upload" action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="post" enctype="multipart/form-data">
+                <input type="file" name="file" id="file"><br>
+                <?php if($innerPath!=""){?> <input type="text" name="innerPath" class="hide" value=<?php echo rawurlencode($innerPath) ?>><?php }?>
+                <input type="submit" value="Upload">
+                <?php
+                # Upload
+                if (isset($_FILES["file"])) {
+                    $file = $_FILES["file"];
+                    // $innerPath = $_POST["innerPath"]
+                    echo IOUtil::saveFile($username, $file, $innerPath) ? '' : 'An Error Occured!';
+                }
+                ?>
+            </form>
+
+            <form id="mkdir" action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="post">
+                <?php if($innerPath!=""){?> <input type="text" name="innerPath" class="hide" value=<?php echo rawurlencode($innerPath) ?>><?php }?>
+                <input type="text" name="dirName" placeholder=<?php
+                                                                # mkdir
+                                                                if (isset($_POST["mkdir"]) && $_POST["mkdir"] == 1) {
+                                                                    $dirName = $_POST["dirName"];
+                                                                    if (!preg_match('/^[\w_\.\-]+$/', $dirName) || $dirName == "") {
+                                                                        echo "InvalidName!";
+                                                                    } else {
+                                                                        if (IOUtil::mkdir($username, $dirName, $innerPath)) {;
+                                                                        } else {
+                                                                            echo "ErrorOccured!";
+                                                                        }
+                                                                    }
+                                                                } else {
+                                                                    echo "FolderName";
+                                                                }
+                                                                ?>><br>
+                <input type="text" name="mkdir" value="1" class="hide">
+                <input type="submit" value="Create Folder">
+            </form>
+
+            <?php
+            if ($innerPath != "") { ?>
+                <div class="fileCard">
+                    <img src='res/icon_back.png' alt="icon"  style="zoom:100%;"><br><br><br>
+                    <form class="fc" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+                        <input type="text" name="innerPath" class="hide" value=<?php
+                                                                            $outPath = substr($innerPath, 0, strrpos($innerPath, '/'));
+                                                                            $outPath = substr($outPath, 0, strrpos($outPath, '/'));
+                                                                            echo $outPath == "" ? rawurlencode("") : rawurlencode($outPath) . '/' ?>>
+                        <input class="btn" type="submit" value="Back" style="margin-left:70px;">
+                    </form>
+                </div>
+            <?php }
+
             foreach (IOUtil::listUserFiles($username, $innerPath) as $idx => $filename) {
                 $filePath = $containerPath . $filename;
                 $relativePath = $innerPath . $filename;
-                if ($innerPath == "") {
-                    if ($filename == "." || $filename == "..") {
-                        continue;
-                    }
+                // if ($innerPath == "") {
+                if ($filename == "." || $filename == "..") {
+                    continue;
                 }
+                // }
             ?>
-                <div id="fileCard">
-                    <img src=<?php if (is_dir($filePath)) echo "res/icon_folder.gif";
-                                else echo "res/icon_file.gif"; ?> alt="" style="zoom:20%;">
+                <div class="fileCard">
+                    <img alt="icon" src=<?php if (is_dir($filePath)) echo "res/icon_folder.gif";
+                                else echo "res/icon_file.gif"; ?>  style="zoom:20%;">
                     <p><?php echo $filename ?></p>
+                    <?php if (!is_dir($filePath)) { ?>
+                        <!-- File Exclusive -->
+                        <form class="fc" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+                            <input type="text" name="filePath" class="hide" value=<?php echo rawurlencode($relativePath) ?>>
+                            <input type="text" name="preview" class="hide" value="1">
+                            <input class="btn" type="submit" value="Preview">
+                        </form>
+                        <form class="fc" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+                            <input type="text" name="filePath" class="hide" value=<?php echo rawurlencode($relativePath) ?>>
+                            <input type="text" name="download" class="hide" value="1">
+                            <input class="btn" type="submit" value="Download">
+                        </form>
+                    <?php } else { ?>
+                        <!-- Folder Exclusive -->
+                        <form class="fc" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+                            <input type="text" name="innerPath" class="hide" value=<?php echo rawurlencode($relativePath . '/') ?>>
+                            <input class="btn" type="submit" value="Open">
+                        </form>
+                    <?php } ?>
+                    <!-- Common -->
                     <form class="fc" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
-                        <input type="text" name="filePath" id="hide" value=<?php echo rawurlencode($relativePath) ?>>
-                        <input type="text" name="preview" id="hide" value="1">
-                        <input id="btn" type="submit" value="Preview">
+                        <input type="text" name="filePath" class="hide" value=<?php echo rawurlencode($relativePath) ?>>
+                        <?php if($innerPath!=""){?> <input type="text" name="innerPath" class="hide" value=<?php echo rawurlencode($innerPath) ?>><?php }?>
+                        <input type="text" name="delete" class="hide" value="1">
+                        <input class="btn" type="submit" value="Delete">
                     </form>
-                    <form class="fc" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
-                        <input type="text" name="filePath" id="hide" value=<?php echo rawurlencode($relativePath) ?>>
-                        <input type="text" name="download" id="hide" value="1">
-                        <input id="btn" type="submit" value="Download">
-                    </form>
-                    <form class="fc" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
-                        <input type="text" name="filePath" id="hide" value=<?php echo rawurlencode($relativePath) ?>>
-                        <input type="text" name="delete" id="hide" value="1">
-                        <input id="btn" type="submit" value="Delete">
-                    </form>
-
+                    <?php
+                    $mime = $finfo->file($filePath);
+                    if ($mime == "application/zip") { ?>
+                    <!-- Unzip -->
+                        <form class="fc" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+                            <?php if($innerPath!=""){?> <input type="text" name="innerPath" class="hide" value=<?php echo rawurlencode($innerPath) ?>><?php }?>
+                            <input type="text" name="filePath" class="hide" value=<?php echo rawurlencode($relativePath) ?>>
+                            <input type="text" name="unzip" class="hide" value="1">
+                            <input class="btn" type="submit" value="Unzip">
+                        </form>
+                    <?php } ?>
                 </div>
             <?php
             }
