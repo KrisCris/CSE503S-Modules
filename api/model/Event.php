ea3fe842288ef5855a59a055bf1a60f230452767
Pingchuan Huang <hi.imhpc@outlook.com>
2021-10-24 21:07:14 -0500

    /event/delEvent.php::uid, token, eid

--- api/model/Event.php
+++ api/model/Event.php
@@ -20,18 +20,26 @@ class Event{
 
     // fetch detail of an event
     public static function getEventById(){
-
+        
     }
 
     // fetch events in a period, say, a month.
     public static function getEventByRange(){
         global $conn;
-        // $stmt = $conn->prepare("select id, title, detail, ")
+        $stmt = $conn->prepare("select id, title, detail, ");
+        if (!$stmt) {
+            printf("Query Prep Failed: %s\n", $conn->error);
+            exit;
+        }
     }
 
     public static function addEvent($uid, $cid, $gid, $title, $detail, $isFullDay, $start, $end=null){
         global $conn;
         $stmt = $conn->prepare("insert into event (uid, cid, gid, title, detail, isFullDay, start, end) values (?,?,?,?,?,?,?,?)");
+        if (!$stmt) {
+            printf("Query Prep Failed: %s\n", $conn->error);
+            exit;
+        }
         $stmt->bind_param('iiissiii', $uid, $cid, $gid, $title, $detail, $isFullDay, $start, $end);
         if($stmt->execute()){
             $id = $conn->insert_id;
@@ -40,5 +48,21 @@ class Event{
         }
         $stmt->close();
         return null;
+    }
+
+    public static function removeEvent($id){
+        global $conn;
+        $stmt = $conn->prepare("delete from event where id=?");
+        if (!$stmt) {
+            printf("Query Prep Failed: %s\n", $conn->error);
+            exit;
+        }
+        $stmt->bind_param('i', $id);
+        if($stmt->execute()){
+            $stmt->close();
+            return true;
+        }
+        $stmt->close();
+        return false;
     }
 }
