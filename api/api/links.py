5f698fb17facd0dd50bba4a68e809c502a8b33e3
Conn_Lost <hi.imhpc@outlook.com>
2021-11-27 11:13:11 -0600

    refresh links, get links

--- api/api/links.py
+++ api/api/links.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request
 from flask_cors import CORS
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from util.util import reply, get_future_time
+from util.util import reply, get_future_time, get_current_time
 
 links = Blueprint(name='links', import_name=__name__, url_prefix='/links')
 CORS(links)
@@ -50,6 +50,23 @@ def add_link():
         return reply(500, data={"err": str(ex)}), 500
 
 
+@links.route('/', methods=['GET'])
+@jwt_required()
+def getAllLinks():
+    try:
+        from app import db
+        result = list(db.links.find({'uid': get_jwt_identity()}))
+        for link in result:
+            link['_id'] = str(link['_id'])
+            link['expired'] = True if get_current_time() > link['expiry'] else False
+        return reply(1, data=result)
+
+    except Exception as ex:
+        if hasattr(ex, 'code'):
+            return reply(ex.code, msg=str(ex)), ex.code
+        return reply(500, data={"err": str(ex)}), 500
+
+
 @links.route('/<shortened>', methods=['GET'])
 def parse_link(shortened):
     try:
@@ -58,6 +75,8 @@ def parse_link(shortened):
         idx = decoded[0] if len(decoded) else None
         result = list(db.links.find({'idx': idx}))
         if len(result):
+            if get_current_time() > result[0]['expiry']:
+                return reply(-3), 202
             return reply(code=1, data={'link': result[0]['original']}), 200
 
         return reply(-1), 404
@@ -76,6 +95,27 @@ def delete_link(lid):
         if dbResp.deleted_count == 1:
             return reply(1), 200
 
+        return reply(-1), 404
+    except Exception as ex:
+        if hasattr(ex, 'code'):
+            return reply(ex.code, msg=str(ex)), ex.code
+        return reply(500, data={"err": str(ex)}), 500
+
+
+@links.route('/<lid>', methods=['PATCH'])
+@jwt_required()
+def refresh_link(lid):
+    try:
+        from app import db
+        t = get_future_time(7)
+        dbResp = db.links.update_one(
+            {'_id': ObjectId(lid)},
+            {'$set': {'expiry': t}}
+        )
+        if dbResp.modified_count:
+            return reply(1, data={'newExpiry': t}), 200
+        if dbResp.matched_count:
+            return reply(1), 202
         return reply(-1), 404
     except Exception as ex:
         if hasattr(ex, 'code'):
