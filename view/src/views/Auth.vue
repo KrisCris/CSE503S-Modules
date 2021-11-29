0d3cc364ba5c741700d604466ce577c83882acbd
Conn_Lost <hi.imhpc@outlook.com>
2021-11-29 08:37:51 -0600

    auth

--- view/src/views/Auth.vue
+++ view/src/views/Auth.vue
@@ -1,61 +1,163 @@
 <template>
-    <h1>AUTH PAGE</h1>
-    <input type="text" v-model="username" />
-    <input type="password" v-model="password" />
-    <button @click="login">login</button>
-    <button @click="register">register</button>
-    <p>
-        {{ userInfo }}
-    </p>
-    <button @click="getInfo">info</button>
+    <form class="sign" @submit.prevent="auth">
+        <h1>Welcome!</h1>
+        <el-alert
+            title="Error"
+            type="error"
+            :description="errorMsg"
+            show-icon
+            v-if="errorMsg"
+            :closable="false"
+            @click="errorMsg=''"
+        >
+        </el-alert>
+
+        <label>Username:</label>
+        <input type="text" v-model="username" required />
+        <p v-if="invalidUsername" class="error">{{ invalidUsername }}</p>
+
+        <label>Password:</label>
+        <input type="password" v-model="password" required />
+        <p v-if="invalidPassword" class="error">{{ invalidPassword }}</p>
+
+        <div v-show="showInvitation">
+            <label>Invitation Code:</label>
+            <input type="text" v-model="invitationCode" />
+        </div>
+        
+
+        <el-radio-group v-model="action">
+            <el-radio-button label="Sign In"></el-radio-button>
+            <el-radio-button label="Sign Up"></el-radio-button>
+        </el-radio-group>
+        <div class="submit">
+            <button>CONTINUE</button>
+        </div>
+    </form>
 </template>
 
 <script>
-import { GET, POST, PUT, PATCH, DELETE } from "../requests.js";
+import { GET, POST } from "../requests.js";
 
 export default {
     name: "Auth",
     data() {
         return {
+            regex: /^[\w_\.\-]+$/,
             username: "",
             password: "",
+            action: "Sign In",
             userInfo: undefined,
+            invalidPassword: "",
+            errorMsg: "",
+            invitationCode: ''
         };
     },
     methods: {
-        login() {
-            if (this.username && this.password) {
-                POST("/users/login", {
+        auth() {
+            let api =
+                this.action == "Sign In" ? "/users/login" : this.invitationCode ? "/users/register/"+this.invitationCode : "/users/register";
+            if (
+                this.username &&
+                this.password &&
+                this.regex.test(this.username)
+            ) {
+                POST(api, {
                     username: this.username,
                     password: this.password,
                 }).then((res) => {
-                    console.log(res);
+                    if (res.code == 1) {
+                        this.$router.push({ name: "Home" });
+                    } else {
+                        this.errorMsg = res.msg;
+                    }
                 });
             } else {
-                alert("invalid input");
+                this.errorMsg = "Invalid Input";
             }
         },
-        register() {
-            if (this.username && this.password) {
-                POST("/users/register", {
-                    username: this.username,
-                    password: this.password,
-                }).then((res) => {
-                    console.log(res);
-                });
-            } else {
-                alert("invalid input");
+    },
+    mounted() {
+        GET("/users/").then((res) => {
+            if (res.code == 1) {
+                this.$router.push({ name: "Home" });
             }
+        });
+    },
+    computed: {
+        showInvitation(){
+            if(this.action == "Sign Up"){
+                return true
+            }
+            return false
         },
-
-        getInfo(){
-            GET('/links/').then((res)=>{
-                this.userInfo = res.data
-            })
-        }
+        invalidUsername() {
+            if (!this.regex.test(this.username) && !this.username == "") {
+                return "Invalid Username!";
+            } else return " ";
+        },
     },
 };
 </script>
 
 <style>
+.sign {
+    max-width: 420px;
+    margin: 5rem auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 2rem;
+
+}
+.sign .radio {
+    margin-top: 1rem;
+}
+.sign label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size:0.8em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+
+.sign button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+
+.sign button:hover {
+    background: #217aff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+
+.sign .submit {
+    text-align: center;
+}
+
+.sign input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+.sign .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+}
 </style>
--- view/src/views/Home.vue
+++ view/src/views/Home.vue
@@ -1,15 +1,123 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <h1>HOME PAGE</h1>
+    <Nav></Nav>
+    <div id="home">
+        <div class="noti">
+            <el-alert
+                v-if="errorMsg"
+                :title="errorMsg"
+                type="error"
+                effect="dark"
+                :closable="false"
+                @click="errorMsg=''"
+            >
+            </el-alert>
+        </div>
+        <form id="homeForm" @submit.prevent="handleSubmit">
+            <input
+                type="text"
+                v-model="originalURL"
+                placeholder="Paste a link here"
+            />
+            <el-button type="primary" round @click="handleSubmit"
+                >Short URL</el-button
+            >
+        </form>
+        <div class="noti">
+            <el-alert v-show="shortURL" :title="shortURL" type="success" effect="dark" :closable="false" @click="shortURL=''"> </el-alert>
+        </div>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { GET, POST, PUT, PATCH, DELETE } from "../requests.js";
+import Nav from "../components/Nav.vue";
 
 export default {
     name: "Home",
-    components: {},
+    components: { Nav },
+    data() {
+        return {
+            baseUrl: "",
+            originalURL: "",
+            errorMsg: "",
+            shortURL: "",
+        };
+    },
+    methods: {
+        handleSubmit() {
+            let url = this.originalURL.toLowerCase();
+            if (!url.startsWith("https://") && !url.startsWith("http://")) {
+                this.originalURL = "https://" + this.originalURL;
+            }
+            PUT("/links/", {
+                link: this.originalURL,
+            }).then((res) => {
+                if (res.code == 1) {
+                    this.updateClipboard(this.baseUrl + "/" + res.data.shortened)
+                    this.shortURL = this.baseUrl + "/" + res.data.shortened + " is copied to your clipboard!";
+                } else {
+                    if(res.code==0){
+                        this.$router.push({ name: "Auth" });
+                    } else {
+                        this.errorMsg = res.msg;
+                    }
+                }
+            });
+        },
+        updateClipboard(newClip) {
+            let that = this
+            navigator.clipboard.writeText(newClip).then(function() {
+                that.successMsg="Copied "+newClip
+            }, function() {
+                errorMsg="No permission to access clipboard!"
+            });
+        }
+    },
+    mounted() {
+        // check login status
+        GET("/users/").then((res) => {
+            if (res.code == -1) {
+                this.$router.push({ name: "Auth" });
+            } else if (res.code == -3) {
+                POST("/user/refresh", {}).then(() => {
+                    window.location.reload;
+                });
+            }
+        });
+
+        let url = window.location.origin.toLowerCase();
+        if (url.startsWith("https://")) {
+            url = url.substring(8, url.length);
+        } else if (url.startsWith("http://")) {
+            url = url.substring(7, url.length);
+        }
+        this.baseUrl = url;
+    },
 };
 </script>
+<style>
+#home {
+    margin-top: 8rem;
+}
+#homeForm {
+    margin: 2rem;
+}
+#homeForm input {
+    width: 40%;
+    height: 3rem;
+    border-radius: 1.5rem;
+    padding: 0px 1rem;
+    font-size: 1.5rem;
+    border: none;
+    margin-right: 1rem;
+}
+#homeForm button {
+    height: 3rem;
+    border-radius: 1.5rem;
+}
+.noti {
+    width: 45%;
+    margin: 0px auto;
+    font-weight: bold;
+}
+</style>
